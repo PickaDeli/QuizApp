71855225e15dccbe855d454a2ba313e97b970b70
Sari <s.meuronen@me.com>
2024-11-15 13:25:07 +0200

    Added Bulma to angular.json and deleted angular material. Made pageNotFound compnent (empty) and made topics.Component. Added folder for images.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { TopicsComponent } from './topics/topics.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'topics', component: TopicsComponent },
+  { path: '**', component: PageNotFoundComponent }
+
+];
---
+++ src/app/topics/topics.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-topics',
+  standalone: true,
+  imports: [],
+  templateUrl: './topics.component.html',
+  styleUrl: './topics.component.css'
+})
+export class TopicsComponent {
+
+}
