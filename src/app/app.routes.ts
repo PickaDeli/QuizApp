9c38e34b95b5e036222b161baf00deb795235f8f
Marianne Salakka <Marianne.Salakka@student.lab.fi>
2024-11-19 13:33:17 +0200

    frontpage update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,13 @@
 import { Routes } from '@angular/router';
 import { TopicsComponent } from './topics/topics.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { FrontPageComponent } from './front-page/front-page.component';
 
 export const routes: Routes = [
+  { path: '', redirectTo: '/frontPage', pathMatch: 'full' },
+  { path: 'frontPage', component: FrontPageComponent },
   { path: 'topics', component: TopicsComponent },
   { path: '**', component: PageNotFoundComponent }
+
 
 ];
--- src/app/front-page/front-page.component.ts
+++ src/app/front-page/front-page.component.ts
@@ -1,9 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-front-page',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './front-page.component.html',
   styleUrl: './front-page.component.css'
 })
