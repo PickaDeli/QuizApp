97e214ee4c3bd9815060bd7a79b8a1884df1876b
Sari <s.meuronen@me.com>
2024-12-05 09:44:51 +0200

    Found the problem from saving the username

--- src/app/front-page/front-page.component.ts
+++ src/app/front-page/front-page.component.ts
@@ -3,28 +3,37 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 import { UserService } from '../services/user.service';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-front-page',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [
+    CommonModule,
+    FormsModule,
+    MatInputModule],
   templateUrl: './front-page.component.html',
   styleUrl: './front-page.component.css'
 })
 export class FrontPageComponent {
+  username: string = '';
 
   constructor(private router: Router, private user: UserService) {
-
+    this.username = 'Testi';
   }
-  username: string = '';
-  ComputerSienceService: any;
+
+
 
   OnSubmit(): void {
-    console.log('Osuu ja uppoo', this.username);
+
     if (this.username) {
+      console.log('Osuu ja uppoo', this.username);
       this.user.setUserName(this.username);  // Tallenna käyttäjänimi UserServiceen
       this.router.navigate(['/topics']);  // Siirry aiheen valintanäkymään
       console.log('topics toimii')
+    }
+    else {
+      console.log('Error')
     }
   }
 }
