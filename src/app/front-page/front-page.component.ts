8832debc6dcaf8bf828899077a076b87d4ddffb1
Marianne Salakka <Marianne.Salakka@student.lab.fi>
2024-12-05 08:53:55 +0200

    userService created, frontpage update

--- src/app/front-page/front-page.component.ts
+++ src/app/front-page/front-page.component.ts
@@ -1,7 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
+import { Router } from '@angular/router';
+import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-front-page',
@@ -11,13 +12,19 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './front-page.component.css'
 })
 export class FrontPageComponent {
+
+  constructor(private router: Router, private user: UserService) {
+
+  }
+  username: string = '';
   ComputerSienceService: any;
 
-  OnSubmit(form: any): void {
-    const username = form.value.username;
-    if (username) {
-      this.ComputerSienceService.setUsername(username);  // Tallenna käyttäjänimi ComputerSienceServiceen
-      //this.router.navigate(['/topics']);  // Siirry aiheen valintanäkymään
+  OnSubmit(): any {
+
+    if (this.username) {
+      this.user.setUserName(this.username);  // Tallenna käyttäjänimi UserServiceen
+      this.router.navigate(['/topics']);  // Siirry aiheen valintanäkymään
+      console.log('topics toimii')
     }
   }
 }
