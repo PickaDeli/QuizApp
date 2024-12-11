e18827dfe2dc0f0ff2854fc2f89c485c6500a88a
Sari <s.meuronen@me.com>
2024-12-11 11:29:39 +0200

    Created general knowledge. Works perfectly!

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,15 +3,17 @@ import { TopicsComponent } from './topics/topics.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { FrontPageComponent } from './front-page/front-page.component';
 import { ComputerSienceComponent } from './computer-sience/computer-sience.component';
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { StarrySkyComponent } from './starry-sky/starry-sky.component';
 import { FinalPageComponent } from './final-page/final-page.component';
+import { GeneralComponent } from './general/general.component';
 
 
 export const routes: Routes = [
   { path: '', redirectTo: '/frontPage', pathMatch: 'full' },
   { path: 'computer-sience', component: ComputerSienceComponent },
+  { path: 'general', component: GeneralComponent },
   { path: 'starrySky', component: StarrySkyComponent },
   { path: 'frontPage', component: FrontPageComponent },
   { path: 'topics', component: TopicsComponent },
