f91534bcf5b133b262dc4660b257ff280e215fc2
Marianne Salakka <Marianne.Salakka@student.lab.fi>
2024-12-11 12:54:27 +0200

    created cartoons and geography components. Not yet in topics.

---
+++ src/app/cartoons/cartoons.component.ts
@@ -0,0 +1,66 @@
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { QuestionService } from '../services/question.service';
+import { ScoreService } from '../services/score.service';
+
+@Component({
+  selector: 'app-cartoons',
+  standalone: true,
+  imports: [
+    CommonModule,
+    FormsModule],
+  providers: [HttpClient],
+  templateUrl: './cartoons.component.html',
+  styleUrl: './cartoons.component.css'
+})
+export class CartoonsComponent implements OnInit {
+  //
+  constructor(
+    public questionService: QuestionService,
+    public scoreService: ScoreService) { }
+
+  apiUrl = 'https://opentdb.com/api.php?amount=15&category=32&difficulty=easy'
+  questions: any[] = [];
+  currentQuestion: any = null;
+  isAnswerCorrect: boolean | null = null;
+  selectedAnswer: string = '';
+
+  // Loads questions
+  ngOnInit(): void {
+    this.questionService.loadQuestions(this.apiUrl).subscribe({
+      next: (questions) => {
+        console.log('Questions loaded in component:', questions);
+      },
+      error: (err) => {
+        console.error('Error loading questions in component:', err);
+      }
+    });
+  }
+
+  //  Gets all answers
+  getAllAnswers(): string[] {
+    return this.questionService.getAllAnswers();
+  }
+
+  // Gets the selected answer and checks if it is correct or not
+  selectAnswer(answer: string): void {
+    this.questionService.selectAnswer(answer);
+    if (this.questionService.isAnswerCorrect === true) {
+      this.scoreService.addPoints(1000);
+      console.log('You got +1000 points! Your total score is: ' + this.scoreService.score)
+    }
+  }
+
+  // for tracking the buttons so that they wont switch places after selecting the answer
+  trackByAnswer(index: number, answer: string): string {
+    return answer;
+  }
+
+
+  // Loads the next question
+  nextQuestion(): void {
+    this.questionService.nextQuestion();
+  }
+}
--- src/app/topics/topics.component.ts
+++ src/app/topics/topics.component.ts
@@ -1,12 +1,5 @@
-<<<<<<< HEAD
-import { Component, Input } from '@angular/core';
-
-
-
-=======
 import { Component } from '@angular/core';
 import { UserService } from '../services/user.service';
->>>>>>> e18827dfe2dc0f0ff2854fc2f89c485c6500a88a
 
 
 @Component({
@@ -17,9 +10,6 @@ import { UserService } from '../services/user.service';
   styleUrl: './topics.component.css'
 })
 export class TopicsComponent {
-<<<<<<< HEAD
-  @Input() username: string = '';
-=======
   username: string = '';
 
   constructor(private UserService: UserService) { }
@@ -28,5 +18,4 @@ export class TopicsComponent {
     this.username = this.UserService.getUserName();
   }
 
->>>>>>> e18827dfe2dc0f0ff2854fc2f89c485c6500a88a
 }
