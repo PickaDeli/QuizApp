ab33ac6280a3ffda19fa9d78526ba1ced6e0cfb3
Marianne Salakka <Marianne.Salakka@student.lab.fi>
2025-01-10 14:12:02 +0200

    topscores

--- src/app/final-page/final-page.component.ts
+++ src/app/final-page/final-page.component.ts
@@ -1,28 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, SimpleChanges } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { ScoreService } from '../services/score.service';
+import { CommonModule } from '@angular/common';
+import { ChangeDetectorRef } from '@angular/core';
+
+interface PlayerScore {
+  score: number;
+  date: Date;
+}
 
 @Component({
   selector: 'app-final-page',
   standalone: true,
   imports: [RouterLink,
-    RouterLinkActive
+    RouterLinkActive, CommonModule
   ],
   templateUrl: './final-page.component.html',
   styleUrl: './final-page.component.css'
 })
-export class FinalPageComponent {
+export class FinalPageComponent implements OnInit {
 
   totalscore: number = 0;
+  topScores: PlayerScore[] = [];
 
-  constructor(private scoreService: ScoreService) { }
+  constructor(private scoreService: ScoreService, private cdRef: ChangeDetectorRef) { }
 
   ngOnInit(): void {
-    // Hae kokonaispisteet pelin päätyttyä
-    this.totalscore = this.scoreService.getScore();
+    this.totalscore = this.scoreService.getScore();// Haetaan kokonaispisteet pelin päätyttyä
     console.log('Your score is:', this.totalscore)
+
+    this.topScores = this.scoreService.getTopScores();
   }
-
-
+  endRound(): void {
+    this.scoreService.endRound(); // Lisätään kierroksen kokonaispisteet top 5
+    this.topScores = this.scoreService.getTopScores(); // Päivitä top 5
+    this.cdRef.detectChanges(); // Pakotetaan Angular päivittämään näkymä
+  }
+  updateTopScores(): void {
+    this.topScores = this.scoreService.getTopScores();  // Haetaan top 5 pisteet serviceltä
+  }
 }
 
--- src/app/services/score.service.ts
+++ src/app/services/score.service.ts
@@ -1,6 +1,11 @@
 import { Injectable } from '@angular/core';
 import confetti from 'canvas-confetti';
 
+interface PlayerScore {
+  score: number;
+  date: Date;// Pisteet tallennetaan aikaleimalla, jotta voidaan lajitella pelisessioiden mukaan
+}
+
 // install following:
 //npm install canvas-confetti
 // Alla olevaa ei ehkä tarvitse?
@@ -11,15 +16,36 @@ import confetti from 'canvas-confetti';
 })
 export class ScoreService {
 
-  public score: number = 0;
+  public scores: PlayerScore[] = []; // List of top 5
+  public score: number = 0;   // Player's current score
+
 
   constructor() { }
+
+  endRound(): void {
+
+    if (this.score > 0) {
+      this.scores.push({ score: this.score, date: new Date() });
+      this.scores.sort((a, b) => b.date.getTime() - a.date.getTime());//laskeva järjestys aikaleiman mukaan
+
+      if (this.scores.length > 5) {//top 5
+        this.scores.pop();
+      }
+
+      this.triggerConfetti(); // Käynnistä konfetti
+      this.resetScore();  // Nollaa pistemäärä seuraavaa kierrosta varten
+    }
+  }
 
   // Adds points and triggers the confetti
   addPoints(points: number): number {
     this.score += points;
     this.triggerConfetti();
     return this.score;
+  }
+
+  getTopScores(): PlayerScore[] {
+    return this.scores;
   }
 
   getScore(): number {
